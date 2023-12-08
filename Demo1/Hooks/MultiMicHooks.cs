01b81f23bd00b9a22f253e94996085ae72fc2c8d
Ram <ram@abc.com>
2023-12-08 18:25:38 +0530

    Multimic Demo mode Feature

---
+++ Demo1/Hooks/MultiMicHooks.cs
@@ -0,0 +1,46 @@
+﻿using TechTalk.SpecFlow;
+using AventStack.ExtentReports.Gherkin.Model;
+using Demo1.Utility;
+
+namespace Smart3dTest.Hooks
+{
+    [Binding]
+    public sealed class Smart3dHook : ControlHelper
+    {
+        static ControlHelper help = new();
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+
+
+            ExtentReportInit();
+
+        }
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+             InitilizeAppium();
+            _feature = _extentReports.CreateTest<Feature>(featureContext.FeatureInfo.Title);
+        }
+
+        [BeforeScenario]
+        public void BeforeScenario(ScenarioContext scenarioContext)
+        {
+
+            ControlHelper._driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _scenario = _feature.CreateNode<Scenario>(scenarioContext.ScenarioInfo.Title);
+        }
+
+        [BeforeStep]
+        public void BeforeStep(ScenarioContext scenarioContext)
+        {
+            AddStep(scenarioContext);
+        }
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            TerminateAppium();
+            ExtentReportTeardown();
+        }
+    }
+}
